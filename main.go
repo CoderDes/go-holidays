e675a5a84ac5ff7d27fadbcc3046ce9418757dd5
Eugene <eugzubv@gmail.com>
2020-05-17 18:36:00 +0300

    unmarshal data

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 func main() {
@@ -19,10 +21,51 @@ func main() {
 
 	scanner := bufio.NewScanner(resp.Body)
 
-	var result string
+	var jsonEncodedHolidays string
 	for scanner.Scan() {
-		result += scanner.Text()
+		jsonEncodedHolidays += scanner.Text()
 	}
-	fmt.Println(result)
 
+	decodedHolidays := decodeHolidays(jsonEncodedHolidays)
+
+	currentTime := getCurrentDate()
+	fmt.Println(currentTime)
+
+	fmt.Printf("======= %T", decodedHolidays)
 }
+
+// TODO: define current date
+func getCurrentDate() time.Time {
+	dt := time.Now()
+	return dt
+}
+
+// TODO: loop over the each holiday and define date
+func decodeHolidays(holidays string) interface{} {
+	type Holiday struct {
+		Date        string
+		LocalName   string
+		Name        string
+		CountryCode string
+		Fixed       bool
+		Global      bool
+		Counties    interface{}
+		LaunchYear  interface{}
+		Type        string
+	}
+	bs := []byte(holidays)
+
+	var holidaysDecoded []Holiday
+
+	err := json.Unmarshal(bs, &holidaysDecoded)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return holidaysDecoded
+}
+
+// TODO: compare two dates
+// TODO: check if holiday date is on Fri or Mon
+// TODO: calc the whole length of holidays
