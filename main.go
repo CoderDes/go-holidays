d7c9b228e52db4f23a06ff681fe2eceb90832b6e
Eugene <eugzubv@gmail.com>
2020-05-18 12:46:08 +0300

    add saturday case and output a conclusion

--- main.go
+++ main.go
@@ -17,8 +17,6 @@ func main() {
 
 	defer resp.Body.Close()
 
-	fmt.Println("RESPONSE STATUS:", resp.Status)
-
 	scanner := bufio.NewScanner(resp.Body)
 
 	var jsonEncodedHolidays string
@@ -62,29 +60,54 @@ func decodeHolidays(holidays string) []Holiday {
 		fmt.Println(err)
 	}
 	// ================ DELETE
-	testStruct := Holiday{
-		Date:        "2020-05-22",
-		LocalName:   "TEST Friday",
-		Name:        "TEST Friday",
-		CountryCode: "UA",
-		Fixed:       false,
-		Global:      false,
-		Counties:    nil,
-		LaunchYear:  nil,
-		Type:        "TEST TYPE",
-	}
-	testSlice := []Holiday{testStruct}
-	for _, holiday := range holidaysDecoded {
-		testSlice = append(testSlice, holiday)
-	}
-	holidaysDecoded = testSlice
+	// testStruct := Holiday{
+	// 	Date:        "2020-05-21",
+	// 	LocalName:   "TEST THURSDAY",
+	// 	Name:        "TEST THURSDAY",
+	// 	CountryCode: "UA",
+	// 	Fixed:       false,
+	// 	Global:      false,
+	// 	Counties:    nil,
+	// 	LaunchYear:  nil,
+	// 	Type:        "TEST TYPE",
+	// }
+	// testStruct := Holiday{
+	// 	Date:        "2020-05-19",
+	// 	LocalName:   "TEST ORDINARY",
+	// 	Name:        "TEST ORDINARY",
+	// 	CountryCode: "UA",
+	// 	Fixed:       false,
+	// 	Global:      false,
+	// 	Counties:    nil,
+	// 	LaunchYear:  nil,
+	// 	Type:        "TEST TYPE",
+	// }
+	// testStruct := Holiday{
+	// 	Date:        "2020-05-23",
+	// 	LocalName:   "TEST SATURDAY",
+	// 	Name:        "TEST SATURDAY",
+	// 	CountryCode: "UA",
+	// 	Fixed:       false,
+	// 	Global:      false,
+	// 	Counties:    nil,
+	// 	LaunchYear:  nil,
+	// 	Type:        "TEST TYPE",
+	// }
+	// testSlice := []Holiday{testStruct}
+	// for _, holiday := range holidaysDecoded {
+	// 	testSlice = append(testSlice, holiday)
+	// }
+	// holidaysDecoded = testSlice
 	// ======================== DELETE
 	return holidaysDecoded
 }
 
 func checkHolidays(holidays []Holiday, currentDate time.Time) {
 	const layoutISO = "2006-01-02"
+	var isToday bool
+	var closestHoliday Holiday
 	var holidayLength float64
+	var dateRange string
 
 	for _, holiday := range holidays {
 		holidayDate, err := time.Parse(layoutISO, holiday.Date)
@@ -93,41 +116,75 @@ func checkHolidays(holidays []Holiday, currentDate time.Time) {
 			fmt.Println(err)
 		}
 		if currentDate.Equal(holidayDate) {
-			fmt.Println("TODAY IS A HOLIDAY", holiday)
-			holidayLength = calcHolidayLength(holidayDate)
-			fmt.Printf("Holiday length is %v days", holidayLength)
+			isToday = true
+			// TODO: try to DRY
+			closestHoliday = holiday
+			holidayLength, dateRange = calcHolidayLength(holidayDate)
 			break
 		}
 		if currentDate.Before(holidayDate) {
-			fmt.Println("TODAY IS:", currentDate)
-			fmt.Println("NEXT HOLIDAY IS:", holiday)
-			holidayLength = calcHolidayLength(holidayDate)
-			fmt.Printf("Holiday length is %v days", holidayLength)
+			// TODO: try to DRY
+			closestHoliday = holiday
+			holidayLength, dateRange = calcHolidayLength(holidayDate)
 			break
 		}
 	}
+	conclusion(closestHoliday, holidayLength, dateRange, isToday)
 }
 
-func calcHolidayLength(holidayDate time.Time) float64 {
+func calcHolidayLength(holidayDate time.Time) (float64, string) {
 	const hoursInADay = 24
 	var holidayStart time.Time
 	var holidayEnd time.Time
 	var holidayDays float64
+	var dateRange string
 
 	holidayWeekday := holidayDate.Weekday()
-
+	// TODO: DRY IT !!!
 	switch holidayWeekday.String() {
 	case "Monday":
 		holidayStart = holidayDate.AddDate(0, 0, -2)
 		holidayEnd = holidayDate.AddDate(0, 0, 1)
+		// TODO: to separate func
+		dateRange = fmt.Sprintf("From %v %v to %v %v", holidayStart.Day(), holidayStart.Month(), holidayEnd.Day(), holidayEnd.Month())
 		holidayDays = holidayEnd.Sub(holidayStart).Hours() / hoursInADay
 	case "Friday":
 		holidayEnd = holidayDate.AddDate(0, 0, 3)
+		// TODO: to separate func
+		dateRange = fmt.Sprintf("From %v %v to %v %v", holidayStart.Day(), holidayStart.Month(), holidayEnd.Day(), holidayEnd.Month())
 		holidayDays = holidayEnd.Sub(holidayDate).Hours() / hoursInADay
+	case "Saturday":
+		holidayStart = holidayDate
+		holidayEnd = holidayDate.AddDate(0, 0, 3)
+		// TODO: to separate func
+		dateRange = fmt.Sprintf("From %v %v to %v %v", holidayStart.Day(), holidayStart.Month(), holidayEnd.Day(), holidayEnd.Month())
+		holidayDays = holidayEnd.Sub(holidayStart).Hours() / hoursInADay
 	case "Sunday":
 		holidayStart = holidayDate.AddDate(0, 0, -1)
 		holidayEnd = holidayDate.AddDate(0, 0, 2)
+		// TODO: to separate func
+		dateRange = fmt.Sprintf("From %v %v to %v %v", holidayStart.Day(), holidayStart.Month(), holidayEnd.Day(), holidayEnd.Month())
 		holidayDays = holidayEnd.Sub(holidayStart).Hours() / hoursInADay
+	default:
+		holidayEnd = holidayDate.AddDate(0, 0, 1)
+		dateRange = fmt.Sprintf("on %v of %v", holidayDate.Day(), holidayDate.Month())
+		holidayDays = holidayEnd.Sub(holidayDate).Hours() / hoursInADay
 	}
-	return holidayDays
+	return holidayDays, dateRange
+}
+
+func conclusion(holiday Holiday, lengthInDays float64, dateRange string, isToday bool) {
+	todayOrNot := "The closest holiday"
+	dayOrDays := "days"
+
+	if isToday {
+		todayOrNot = "Today"
+	}
+	if lengthInDays == 1 {
+		dayOrDays = "day"
+	}
+
+	output := fmt.Sprintf("%v is a %v on %v. It will last %v %v: %v.", todayOrNot, holiday.Name, holiday.Date, lengthInDays, dayOrDays, dateRange)
+
+	fmt.Println(output)
 }
