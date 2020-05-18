77d833538c98d046c09de064ba4282816e8034b4
Eugene <eugzubv@gmail.com>
2020-05-18 11:41:48 +0300

    calc holidays' length adjacent to weekends

--- main.go
+++ main.go
@@ -61,29 +61,73 @@ func decodeHolidays(holidays string) []Holiday {
 	if err != nil {
 		fmt.Println(err)
 	}
-
+	// ================ DELETE
+	testStruct := Holiday{
+		Date:        "2020-05-22",
+		LocalName:   "TEST Friday",
+		Name:        "TEST Friday",
+		CountryCode: "UA",
+		Fixed:       false,
+		Global:      false,
+		Counties:    nil,
+		LaunchYear:  nil,
+		Type:        "TEST TYPE",
+	}
+	testSlice := []Holiday{testStruct}
+	for _, holiday := range holidaysDecoded {
+		testSlice = append(testSlice, holiday)
+	}
+	holidaysDecoded = testSlice
+	// ======================== DELETE
 	return holidaysDecoded
 }
 
 func checkHolidays(holidays []Holiday, currentDate time.Time) {
 	const layoutISO = "2006-01-02"
+	var holidayLength float64
+
 	for _, holiday := range holidays {
 		holidayDate, err := time.Parse(layoutISO, holiday.Date)
+
 		if err != nil {
 			fmt.Println(err)
 		}
 		if currentDate.Equal(holidayDate) {
 			fmt.Println("TODAY IS A HOLIDAY", holiday)
+			holidayLength = calcHolidayLength(holidayDate)
+			fmt.Printf("Holiday length is %v days", holidayLength)
 			break
 		}
 		if currentDate.Before(holidayDate) {
 			fmt.Println("TODAY IS:", currentDate)
 			fmt.Println("NEXT HOLIDAY IS:", holiday)
+			holidayLength = calcHolidayLength(holidayDate)
+			fmt.Printf("Holiday length is %v days", holidayLength)
 			break
 		}
-
 	}
 }
 
-// TODO: check if holiday date is on Fri or Mon
-// TODO: calc the whole length of holidays
+func calcHolidayLength(holidayDate time.Time) float64 {
+	const hoursInADay = 24
+	var holidayStart time.Time
+	var holidayEnd time.Time
+	var holidayDays float64
+
+	holidayWeekday := holidayDate.Weekday()
+
+	switch holidayWeekday.String() {
+	case "Monday":
+		holidayStart = holidayDate.AddDate(0, 0, -2)
+		holidayEnd = holidayDate.AddDate(0, 0, 1)
+		holidayDays = holidayEnd.Sub(holidayStart).Hours() / hoursInADay
+	case "Friday":
+		holidayEnd = holidayDate.AddDate(0, 0, 3)
+		holidayDays = holidayEnd.Sub(holidayDate).Hours() / hoursInADay
+	case "Sunday":
+		holidayStart = holidayDate.AddDate(0, 0, -1)
+		holidayEnd = holidayDate.AddDate(0, 0, 2)
+		holidayDays = holidayEnd.Sub(holidayStart).Hours() / hoursInADay
+	}
+	return holidayDays
+}
