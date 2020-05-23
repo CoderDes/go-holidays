39e93d5c88d8ebf40dc31567d16f9ab04367e68b
Eugene <eugzubv@gmail.com>
2020-05-23 15:39:00 +0300

    fix queries

--- employees/employees.go
+++ employees/employees.go
@@ -34,11 +34,12 @@ func ExamineEmployees() {
 			question: "Find all current managers with their title, first/last name, current salary. Continue? y or n: ",
 		},
 		{
-			query:    "SELECT dept_name, title, first_name, last_name, EXTRACT(YEAR FROM dept_emp.to_date) - EXTRACT(YEAR FROM dept_emp.from_date) AS how_may_years FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no INNER JOIN departments ON dept_emp.dept_no = departments.dept_no INNER JOIN titles ON employees.emp_no = titles.emp_no INNER JOIN salaries ON employees.emp_no = salaries.emp_no;",
+			query:    "SELECT DISTINCT dept_name, title, first_name, last_name, hire_date, EXTRACT(YEAR FROM dept_emp.to_date) - EXTRACT(YEAR FROM dept_emp.from_date) AS how_many_years FROM employees INNER JOIN dept_emp ON employees.emp_no = dept_emp.emp_no INNER JOIN departments ON dept_emp.dept_no = departments.dept_no INNER JOIN titles ON employees.emp_no = titles.emp_no INNER JOIN salaries ON employees.emp_no = salaries.emp_no;",
 			question: "Find all employees (title, first/last name, hire date, experience in years). Continue? y or n: ",
 		},
 		{
-			query:    "SELECT COUNT(dept_emp.dept_no) AS employyes_count, SUM(salary) AS salary_sum FROM departments INNER JOIN dept_emp ON departments.dept_no = dept_emp.dept_no INNER JOIN salaries ON dept_emp.emp_no = salaries.emp_no;",
+			// NOTE: there isn't 2020 year in provided db with employyes;
+			query:    "SELECT dept_name, COUNT(dept_emp.emp_no) AS employee_count, SUM(salary) AS salary_sum FROM dept_emp INNER JOIN departments ON dept_emp.dept_no = departments.dept_no INNER JOIN salaries ON dept_emp.emp_no = salaries.emp_no WHERE MONTH(salaries.to_date) = MONTH(CURRENT_DATE()) AND YEAR(salaries.to_date) = YEAR(CURRENT_DATE()) GROUP BY dept_name;",
 			question: "Find all departments, their employee count, sum salary. Continue? y or n: ",
 		},
 	}
